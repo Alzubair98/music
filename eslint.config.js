1665ed59b0ce3c9c5e483b41736b0d16953d6b55
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-10-29 08:33:07 +0300

    add CSS

--- eslint.config.js
+++ eslint.config.js
@@ -17,18 +17,19 @@ export default [
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginCypress.configs.recommended,
     files: [
       'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-      'cypress/support/**/*.{js,ts,jsx,tsx}'
+      'cypress/support/**/*.{js,ts,jsx,tsx}',
     ],
+    env: { node: true },
   },
   skipFormatting,
 ]
