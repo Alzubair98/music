4a8fbb42c87de4c4018a69c260d90319a8f5ec10
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-10-31 14:11:50 +0300

    validate the form and fix the submit button

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -89,6 +89,13 @@
             </button>
           </form>
           <!-- Registration Form -->
+          <div
+            class="text-white text-center font-bold p-4 rounded mb-4"
+            v-if="reg_show_alert"
+            :class="reg_alert_variant"
+          >
+            {{ reg_alert_msg }}
+          </div>
           <vee-form
             v-show="tab === 'register'"
             :validation-schema="schema"
@@ -147,7 +154,6 @@
                   {{ error }}
                 </div></vee-field
               >
-              <ErrorMessage class="text-red-600" name="password" />
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
@@ -190,7 +196,12 @@
 
             <button
               type="submit"
-              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700 cursor-pointer"
+              :disabled="reg_in_submission"
+              :class="{
+                'bg-gray-300 hover:bg-gray-300 cursor-not-allowed':
+                  reg_in_submission,
+              }"
             >
               Submit
             </button>
@@ -222,6 +233,10 @@ export default {
       userData: {
         country: 'USA',
       },
+      reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_variant: 'bg-blue-500',
+      reg_alert_msg: 'Please wait! your account is being created.',
     }
   },
 
@@ -233,6 +248,13 @@ export default {
   },
   methods: {
     register(values) {
+      this.reg_show_alert = true
+      this.reg_in_submission = true
+      this.reg_alert_variant = 'bg-blue-500'
+      this.reg_alert_msg = 'Pleasse wait! Your account is being created.'
+
+      this.reg_alert_variant = 'bg-green-500'
+      this.reg_alert_msg = 'Success! Your account has been created.'
       console.log(values)
     },
   },
