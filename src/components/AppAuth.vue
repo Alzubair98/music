19dc03bb5c10920beeb68a3b21c80007d2834645
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-11-01 05:22:39 +0300

    validate login form

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -62,24 +62,33 @@
           </ul>
 
           <!-- Login Form -->
-          <form v-show="tab === 'login'">
+          <vee-form
+            v-show="tab === 'login'"
+            :validation-schema="schema"
+            @submit="login"
+            :initial-values="userData"
+          >
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
-              <input
+              <vee-field
                 type="email"
+                name="email"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Email"
               />
+              <ErrorMessage class="text-red-600" name="email" />
             </div>
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <input
+              <vee-field
                 type="password"
+                name="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Password"
               />
+              <ErrorMessage class="text-red-600" name="password" />
             </div>
             <button
               type="submit"
@@ -87,7 +96,7 @@
             >
               Submit
             </button>
-          </form>
+          </vee-form>
           <!-- Registration Form -->
           <div
             class="text-white text-center font-bold p-4 rounded mb-4"
@@ -215,6 +224,7 @@
 <script>
 import { mapState, mapWritableState } from 'pinia'
 import useModalStore from '@/stores/modal'
+import { ErrorMessage } from 'vee-validate'
 
 export default {
   name: 'AppAuth',
