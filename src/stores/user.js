52ad927ef175d4978e673ee53f9fa3538eabee89
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-11-02 05:37:25 +0300

    update the uid

--- src/stores/user.js
+++ src/stores/user.js
@@ -7,15 +7,23 @@ export default defineStore('user', {
   }),
   actions: {
     async register(values) {
-      await auth.createUserWithEmailAndPassword(values.email, values.password)
+      const userCred = await auth.createUserWithEmailAndPassword(
+        values.email,
+        values.password,
+      )
 
-      await usersCollection.add({
+      await usersCollection.doc(userCred.user.uid).set({
         name: values.name,
         email: values.email,
         age: values.age,
         singer: values.singer,
         country: values.country,
       })
+
+      await userCred.user.updateProfile({
+        displayName: values.name,
+      })
+
       this.userLoggedIn = true
     },
   },
